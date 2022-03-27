817ccc7294fd599fec4319432a6a7bbebf2643b3
blood8879 <blood8879@gmail.com>
2022-03-27 22:48:50 +0900

    20220327

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { getCartItems, removeCartItem } from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock';
+import { Empty } from 'antd';
+import Paypal from '../../utils/Paypal';
 
 function CartPage(props) {
     const dispatch = useDispatch();
     const [Total, setTotal] = useState(0);
+    const [ShowTotal, setShowTotal] = useState(false)
 
     useEffect(() => {
         
@@ -32,12 +35,15 @@ function CartPage(props) {
         })
 
         setTotal(total);
+        setShowTotal(true)
     }
 
     let removeFromCart = (productId) => {
         dispatch(removeCartItem(productId))
         .then(response => {
-
+            if(response.payload.productInfo.length <= 0) {
+                setShowTotal(false)
+            }
         })
     }
 
@@ -49,9 +55,19 @@ function CartPage(props) {
             <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart} />
         </div>
 
-        <div style={{ marginTop: '3rem' }}>
-            <h2>Total Amount: ${Total}</h2>
-        </div>
+        {ShowTotal ?
+            <div style={{ marginTop: '3rem' }}>
+                <h2>Total Amount: ${Total}</h2>
+            </div>
+            :
+            <>
+            <br />
+            <Empty description={false} />
+            </>
+        }
+
+
+        <Paypal />
         
     </div>
   )
