2359777a2d43f5d4c3249621b07012fc6d3566d9
blood8879 <blood8879@gmail.com>
2022-03-28 23:32:52 +0900

    20220328

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { getCartItems, removeCartItem, onSuccessBuy } from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock';
-import { Empty } from 'antd';
+import { Empty, Result } from 'antd';
 import Paypal from '../../utils/Paypal';
 
 function CartPage(props) {
     const dispatch = useDispatch();
+
     const [Total, setTotal] = useState(0);
     const [ShowTotal, setShowTotal] = useState(false)
+    const [ShowSuccess, setShowSuccess] = useState(false)
 
     useEffect(() => {
         
@@ -48,17 +50,16 @@ function CartPage(props) {
     }
 
     const transactionSuccess = (data) => {
-        
-        
         dispatch(onSuccessBuy({
             paymentData: data,
             cartDetail: props.user.cartDetail
         }))
-        .then(response => {
-            if(response.payload.success) {
-                setShowTotal(false)
-            }
-        })
+            .then(response => {
+                if(response.payload.success) {
+                    setShowTotal(false)
+                    setShowSuccess(true)
+                }
+            })
     }
 
   return (
@@ -73,11 +74,16 @@ function CartPage(props) {
             <div style={{ marginTop: '3rem' }}>
                 <h2>Total Amount: ${Total}</h2>
             </div>
-            :
-            <>
-            <br />
-            <Empty description={false} />
-            </>
+            : ShowSuccess ?
+                <Result 
+                    status="success"
+                    title="Successfully Purchased Items"
+                />
+                :
+                <>
+                    <br />
+                    <Empty description={false} />
+                </>
         }
 
         {ShowTotal &&
--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ const { User } = require("../models/User");
 const { auth } = require("../middleware/auth");
 const { Product } = require('../models/Product');
 const { Payment } = require('../models/Payment');
+const async = require('async');
 
 //=================================
 //             User
@@ -154,7 +155,9 @@ router.get('/removeFromCart', auth, (req, res) => {
 })
 
 router.post('/successBuy', auth, (req, res) => {
-    // 1. User Collection 안에 History 필드 안에 간단한 결제 정보 넣어주기
+
+
+    //1. User Collection 안에  History 필드 안에  간단한 결제 정보 넣어주기
     let history = [];
     let transactionData = {};
 
@@ -169,36 +172,141 @@ router.post('/successBuy', auth, (req, res) => {
         })
     })
 
-    // 2. Payment Collection 안에 자세한 결제 정보들 넣어주기
+    //2. Payment Collection 안에  자세한 결제 정보들 넣어주기 
     transactionData.user = {
         id: req.user._id,
         name: req.user.name,
-        email: req.user.email,
+        email: req.user.email
     }
 
     transactionData.data = req.body.paymentData
     transactionData.product = history
 
-    // history 정보 저장
+    //history 정보 저장 
     User.findOneAndUpdate(
         { _id: req.user._id },
         { $push: { history: history }, $set: { cart: [] } },
         { new: true },
         (err, user) => {
-            if(err) return res.json({ success: false, err })
+            if (err) return res.json({ success: false, err })
 
-            // payment 에다가 transactionData 정보 저장
+
+            //payment에다가  transactionData정보 저장 
             const payment = new Payment(transactionData)
             payment.save((err, doc) => {
-                if(err) return res.json({ success: false, err })
+                if (err) return res.json({ success: false, err })
 
-                // 3. Product Collection 안에 있는 sold 필드 정보 업데이트 시켜주기
-                
+
+                //3. Product Collection 안에 있는 sold 필드 정보 업데이트 시켜주기 
+
+
+                //상품 당 몇개의 quantity를 샀는지 
+
+                let products = [];
+                doc.product.forEach(item => {
+                    products.push({ id: item.id, quantity: item.quantity })
+                })
+
+
+                async.eachSeries(products, (item, callback) => {
+
+                    Product.updateMany(
+                        { _id: item.id },
+                        {
+                            $inc: {
+                                "sold": item.quantity
+                            }
+                        },
+                        { new: false },
+                        callback
+                    )
+                }, (err) => {
+                    if (err) return res.status(400).json({ success: false, err })
+                    res.status(200).json({
+                        success: true,
+                        cart: user.cart,
+                        cartDetail: []
+                    })
+                }
+                )
             })
         }
     )
+})
+
+// router.post('/successBuy', auth, (req, res) => {
+//     // 1. User Collection 안에 History 필드 안에 간단한 결제 정보 넣어주기
+//     let history = [];
+//     let transactionData = {};
+
+//     req.body.cartDetail.forEach((item) => {
+//         history.push({
+//             dateOfPurchase: Date.now(),
+//             name: item.title,
+//             id: item._id,
+//             price: item.price,
+//             quantity: item.quantity,
+//             paymentId: req.body.paymentData.paymentID
+//         })
+//     })
+
+//     // 2. Payment Collection 안에 자세한 결제 정보들 넣어주기
+//     transactionData.user = {
+//         id: req.user._id,
+//         name: req.user.name,
+//         email: req.user.email,
+//     }
+
+//     transactionData.data = req.body.paymentData
+//     transactionData.product = history
+
+//     // history 정보 저장
+//     User.findOneAndUpdate(
+//         { _id: req.user._id },
+//         { $push: { history: history }, $set: { cart: [] } },
+//         { new: true },
+//         (err, user) => {
+//             if(err) return res.json({ success: false, err })
+
+//             // payment 에다가 transactionData 정보 저장
+//             const payment = new Payment(transactionData)
+//             payment.save((err, doc) => {
+//                 if(err) return res.json({ success: false, err })
+
+//                 // 3. Product Collection 안에 있는 sold 필드 정보 업데이트 시켜주기
+
+//                 // 상품당 몇개의 quantity를 샀는지
+//                 let products = [];
+//                 doc.product.forEach(item => {
+//                     products.push({ id: item.id, quantity: item.quantity })
+//                 })
+
+//                 async.eachSeries(products, (item, callback) => {
+//                     Product.updateMany(
+//                         { _id: item.id },
+//                         {
+//                             $inc: {
+//                                 "sold" : item.quantity
+//                             }
+//                         },
+//                         { new : false },
+//                         callback
+//                     ), (err) => {
+//                         if(err) return res.status(400).json({ success: false, err })
+//                         res.status(200).json({
+//                             sucess: true,
+//                             cart: user.cart,
+//                             cartDetail: []
+//                         })
+//                     }
+//                 })
+                
+
+//             })
+//         }
+//     )
 
     
-})
+// })
 
 module.exports = router;
